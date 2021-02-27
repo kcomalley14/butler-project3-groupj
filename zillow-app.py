bac9220bac920fca7456a3c071bb42a3f75f0bb8
Kyle O'Malley <kyleomalley@Kyles-MacBook-Pro.local>
2021-02-27 16:26:30 -0700

    heroku dependencies

---
+++ zillow-app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+st.header("Welcome Home Value Estimator")
+st.subheader('''
+    Directions for the page here:
+        1. We have a dashboard for our PowerBI info
+        2. Data preprocessing and predictions for home values
+    ''')
+
+options = st.selectbox('Please Select',['PowerBI', 'Preprocessing & Predictions'])

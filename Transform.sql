50f7a1f182ba22eede326276a65fc9d669086852
William Taylor <65783939+trippy64@users.noreply.github.com>
2021-03-01 17:18:51 -0500

    Bill files to this point

---
+++ Transform.sql
@@ -0,0 +1,13 @@
+select * from "ZillowData" limit 10
+
+
+ALTER TABLE "ZillowData" 
+renanme  COLUMN "0" to "HomeValue";
+
+
+ALTER TABLE "ZillowData"
+ren COLUMN "0" [SET DATA] TYPE money;
+
+
+ALTER TABLE "ZillowData" 
+renanme  COLUMN "filed_9" to "date";

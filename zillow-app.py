e1e5fe02bf0c03bfa101bf95a1f17aed666b2556
Kyle O'Malley <kyleomalley@Kyles-MBP.lan>
2021-03-06 10:12:31 -0500

    fix error heroku

--- zillow-app.py
+++ zillow-app.py
@@ -25,11 +25,11 @@ if options == 'Tableau Dashboard':
     # st.markdown("""
     #     <iframe width="600" height="606" src="https://public.tableau.com/shared/SGTJGH3CF?:display_count=y&:origin=viz_share_link" frameborder="0" style="border:0" allowfullscreen></iframe>
     #     """, unsafe_allow_html=True)
-    def main():
+    # def main():
         html_temp = """ <div class='tableauPlaceholder' id='viz1614995758021' style='position: automatic'><noscript><a href='#'><img alt=' ' height=600 src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;HV&#47;HV_Dashboard_16149955310770&#47;Sheet1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='HV_Dashboard_16149955310770&#47;Sheet1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;HV&#47;HV_Dashboard_16149955310770&#47;Sheet1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1614995758021');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script> """ 
-        components.html(html_temp)
-    if __name__ == "__main__":
-        main()
+        components.html(html_temp, width= 800, height=600, scrolling=True)
+    # if __name__ == "__main__":
+    #     main()
     
 
 elif options == 'Preprocessing & Predictions: Indiana':
